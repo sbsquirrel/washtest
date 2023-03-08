f4da740c88cd62c867e3f1039fb332faa98f9214
Serge <res.xoffice@gmail.com>
2023-03-08 11:41:00 +0200

    0

--- js/getwashdata0.js
+++ js/getwashdata0.js
@@ -5,16 +5,39 @@
     $(document).ready(function() {
         $("#submitButton").click(function() {
 
-    $.ajaxSetup({
- 		 headers : {   
-    	'X-Sonnys-API-ID' : 'happyswash',
-	'X-Sonnys-API-Key' : '6334661aadd1ab6c8b00035bc5f6a'
-  	}
-	});
-        $.getJSON("https://sandboxapi.sonnyscontrols.com/v1/customer", function(resp) {
-            var feat = resp.data.customers,
-        });
 
-        });
+      console.log("Getting data:");         
+  
+          
+  $.ajax({
+            url: "https://sandboxapi.sonnyscontrols.com/v1/customer",
+            type: 'GET', 
+            crossDomain: true,
+            dataType: 'json',
+            headers : {   
+               'X-Sonnys-API-ID' : 'happyswash',
+               'X-Sonnys-API-Key' : '6334661aadd1ab6c8b00035bc5f6a',
+               'Content-type': 'application/x-www-form-urlencoded',
+               'Accept': 'application/json'
+
+        },
+          
+          //  contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+               // CallBack(result);
+              console.log("OK");
+	   
+            },
+            error: function (error) {
+           
+		console.dir(error);
+            }
+        });          
+                 
+          
+         console.log("Done!");
+  
+
     });
+});
 })();
